a2c991764cefe8efbcc6a2030a6331f666be17c7
Mark Marsala <mmarsala76@gmail.com>
2021-12-23 16:06:05 -0500

    server.js updates

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { fstat } = require("fs");
 const path = require("path");
 const app = express();
 const port = 3001;
@@ -17,4 +18,23 @@ app.get('/notes', (req, res) => {
 
 app.listen(PORT, () => {
     console.log('App listening on PORT ${PORT}');
+});
+
+function createNewNote(body, notesArray) {
+    const note = body;
+    notesArray.push(note);
+    fstat.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify({ notes: notesArray }, null, 2)
+    );
+    return note;
+}
+
+app.get('/api/notes', (req, res) => {
+    return res.json(notes);
+});
+
+app.post('/api/notes', (req, res) => {
+    const note = createNewNote(req.body, notes);
+    res.json(note);
 });
